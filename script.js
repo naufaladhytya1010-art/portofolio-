78af8c28c7b07bac55e93aebe4af89a87e000de0
naufaladhytya1010-art <naufaladhytya1010@gmail.com>
2026-06-21 15:40:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const form = document.getElementById("form");
+const saldoEl = document.getElementById("saldo");
+const riwayatEl = document.getElementById("riwayat");
+
+let transaksi = JSON.parse(localStorage.getItem("transaksi")) || [];
+
+function tampilkanData() {
+    riwayatEl.innerHTML = "";
+
+    let saldo = 0;
+
+    transaksi.forEach(item => {
+        const li = document.createElement("li");
+
+        li.classList.add(item.tipe);
+
+        li.textContent =
+            `${item.nama} - Rp ${item.jumlah.toLocaleString("id-ID")}`;
+
+        riwayatEl.appendChild(li);
+
+        if (item.tipe === "masuk") {
+            saldo += item.jumlah;
+        } else {
+            saldo -= item.jumlah;
+        }
+    });
+
+    saldoEl.textContent =
+        `Rp ${saldo.toLocaleString("id-ID")}`;
+
+    localStorage.setItem(
+        "transaksi",
+        JSON.stringify(transaksi)
+    );
+}
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const nama = document.getElementById("nama").value;
+    const jumlah = parseInt(
+        document.getElementById("jumlah").value
+    );
+    const tipe = document.getElementById("tipe").value;
+
+    transaksi.push({
+        nama,
+        jumlah,
+        tipe
+    });
+
+    tampilkanData();
+    form.reset();
+});
+
+tampilkanData();
